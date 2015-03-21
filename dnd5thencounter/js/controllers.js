09feb7d2c9e50d2fa946d0980b30fcc97fe935dd
Jason Hardin <psychoph@gmail.com>
2015-03-21 11:47:23 -0700

    fixed value for inputs

--- dnd5thencounter/js/controllers.js
+++ dnd5thencounter/js/controllers.js
@@ -5,6 +5,8 @@ var dnd5EncApp  = angular.module('dnd5EncApp', ['ngAria']);
 
 dnd5EncApp.controller('playerCalcCtrl', function($scope){
     $scope.math = 0;
+    $scope.players = 3;
+    $scope.level = 1;
     $scope.calculate = function(){
         $scope.math = $scope.players * $scope.level;
     }
@@ -12,5 +14,11 @@ dnd5EncApp.controller('playerCalcCtrl', function($scope){
 });
 
 dnd5EncApp.controller('monstersCalcCtrl', function($scope){
+    $scope.monsters = 3;
+    $scope.experience = 100;
+    $scope.challengeRating = 1;
 
+    $scope.calculate = function(){
+        $scope.math = $scope.monsterss * $scope.experience;
+    }
 });
