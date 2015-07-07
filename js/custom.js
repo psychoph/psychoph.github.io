869cf1c9b45683c34c9a2fcf9ac86a0ec8598e44
Jason Hardin <psychoph@gmail.com>
2015-07-06 19:24:11 -0700

    fix for slide

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 $(".post-read-more").click(function() {
     $(this).closest(".post-preview").hide();
-    $(this).closest(".post").children('.post-content').slidedown();
+    $(this).closest(".post").children('.post-content').slideToggle();
 });
 
 $('.post-close').click(function() {
-    $(this).closest(".post-content").slideup();
+    $(this).closest(".post-content").slideToggle();
     $(this).closest(".post").children('.post-preview').show();
 });
