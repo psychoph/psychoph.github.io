2f73233c07996e369f75d22c31f25ee92621ab0b
Jason Hardin <psychoph@gmail.com>
2015-07-06 19:29:18 -0700

    close fix and stop click

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,11 @@
 $(".post-read-more").click(function() {
+    event.preventDefault();
     $(this).closest(".post-preview").hide();
     $(this).closest(".post").children('.post-content').slideToggle();
 });
 
 $('.post-close').click(function() {
+    event.preventDefault();
     $(this).closest(".post-content").slideToggle();
     $(this).closest(".post").children('.post-preview').show();
 });
