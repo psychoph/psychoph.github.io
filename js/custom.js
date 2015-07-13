ad2e530c806606b573be7339fd40dcbda5857080
Jason Hardin <psychoph@gmail.com>
2015-07-12 19:39:38 -0700

    test

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,25 @@
-// Requires jQuery of course.
-$(document).ready(function() {
-    $('.show-comments').on('click', function(){
-          var disqus_shortname = 'stringonafinger'; // Replace this value with *your* username.
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'stringonafinger'; // required: replace example with your forum shortname
 
-          // ajax request to load the disqus javascript
-          $.ajax({
-                  type: "GET",
-                  url: "http://" + disqus_shortname + ".disqus.com/embed.js",
-                  dataType: "script",
-                  cache: true
-          });
-          // hide the button once comments load
-          $(this).fadeOut();
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+  var s = document.createElement('script');
+  s.async = true;
+  s.type = 'text/javascript';
+  s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
+
+$(document).ready(function() {
+  $('.show-comments').on('click', function() {
+    // ajax request to load the disqus javascript
+    $.ajax({
+      type: "GET",
+      url: "//" + disqus_shortname + ".disqus.com/embed.js",
+      dataType: "script",
+      cache: true
     });
+    // hide the button once comments load
+    $(this).fadeOut();
+  });
 });
