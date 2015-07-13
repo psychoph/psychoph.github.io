08950a6ce172944f1e87f26eedfbfbbbe10715ee
Jason Hardin <psychoph@gmail.com>
2015-07-12 19:43:53 -0700

    test

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,6 @@
 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 var disqus_shortname = 'stringonafinger'; // required: replace example with your forum shortname
 
-/* * * DON'T EDIT BELOW THIS LINE * * */
-(function() {
-  var s = document.createElement('script');
-  s.async = true;
-  s.type = 'text/javascript';
-  s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-}());
-
 $(document).ready(function() {
   $('.show-comments').on('click', function() {
     // ajax request to load the disqus javascript
